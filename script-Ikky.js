1786f6969192415d56bfcd36601d02ba34103eaa
Ikky David Augusto Teixeira de Sousa <86940081+Oikky@users.noreply.github.com>
2026-09-22 16:46:32 -0300

    Fix inventory reference and update item selection logic

--- script-Ikky.js
+++ script-Ikky.js
@@ -4,8 +4,8 @@
 document.addEventListener("keydown", function (teclado) {
   const numero = Number(teclado.key);
 
-  if (numero >= 1 && numero <= player.invetory.length) {
-    current_item = player.invetory[numero - 1];
+  if (numero >= 1 && numero <= player.inventory.length) {
+    current_item = numero - 1;
     console.log(`Selecionado: ${current_item}`);
   } else {
   }
