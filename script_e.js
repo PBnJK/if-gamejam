9dfa89af5a69ed16704009a3cc16194a7d5303cf
lissaLuxx <153519185+lissaLuxx@users.noreply.github.com>
2026-09-22 21:03:48 -0300

    Add files via upload

--- script_e.js
+++ script_e.js
@@ -21,6 +21,7 @@ const blast_radius_high = 100;
 
 let blast_radius = blast_radius_low;
 
+
 const flashlight_radius_low = 150;
 const flashlight_radius_high = 350;
 let flashlight_radius = flashlight_radius_low;
@@ -34,15 +35,21 @@ class Player {
     this.health = 2;
     this.inventory = [];
   }
+
+  hit (){
+
+    this.health --;
+  }
 }
 
 class Flashlight {
   constructor() {
-    this.battery = max_battery / fps;
-    this.is_on = false;
+    this.battery = max_battery;
+    this.is_on = true;
   }
 
   use_primary() {
+
     play_sound("assets/audio/sfx/on_off_flashlight.mp3");
 
     if (this.is_on == true) {
@@ -54,15 +61,17 @@ class Flashlight {
 
     if (this.battery <= 0) return;
 
-    lights_on();
+    light_on();
     this.is_on = true;
   }
 
   use_secondary() {
+    
     if (this.battery > max_battery || this.is_on == true) return;
     this.battery += fps / 2; // 500 coin batteries
     setTimeout(() => {}, 100 / fps);
     play_sound("assets/audio/sfx/add_battery.mp3");
+
   }
 
   battery_decay() {
@@ -75,10 +84,10 @@ class Flashlight {
     if (this.is_on == false) return;
     const enemies_in_sight = shine(mouse_x, mouse_y, light_radius);
 
-    if (enemies_in_sight.length != 0) {
+    if (enemies_in_sight.length != 0){
       let seen = false;
       for (const i of enemies_in_sight) {
-        if (i.seen) {
+        if( i.seen ) {
           seen = true;
         } else {
           i.seen = true;
@@ -87,12 +96,14 @@ class Flashlight {
         i.hit_by_light();
       }
 
-      if (seen) {
+      if( seen ) {
         play_sound("assets/audio/sfx/scare.mp3");
       }
     }
   }
 }
+
+
 
 class Shotgun {
   constructor() {
@@ -113,17 +124,20 @@ class Shotgun {
       play_sound("assets/audio/sfx/bullet_hit.mp3");
     }
 
-    this.ammo--;
+   this.ammo--;
     this.chambering = 1 * fps;
 
+    
     setTimeout(() => {}, 300 / fps);
     play_sound("assets/audio/sfx/shotgun_shell.mp3");
   }
 
-  chamber_ammo() {
-    if (this.chambering > 0) {
-      this.chambering--;
-    }
+  chamber_ammo(){
+
+    if (this.chambering > 0){
+      this.chambering --;
+  }
+
   }
 
   use_secondary() {
@@ -133,6 +147,8 @@ class Shotgun {
 
       ammo++;
       setTimeout(() => {}, 500 / fps);
+
+      
     }
   }
 }
@@ -143,25 +159,29 @@ class Heal {
     this.quantity = 1;
   }
 
-  use_primary() {
+  use_primary(player) {
     if (this.quantity <= 0) return;
     if (player.health >= max_hp) return;
 
     play_sound("assets/audio/sfx/injection.mp3");
     player.health++;
-    this.quantity--;
+    this.quantity --;
   }
 
   use_secondary() {
-    const enemies_hit = shoot(mouse_x, mouse_y, blast_radius);
-    play_sound("assets/audio/sfx/throw_healing_item.mp3");
 
-    for (const i of enemies_hit) {
-      i.hit(5);
-    }
-    return;
+      enemies_hit = shoot();
+      play_sound("assets/audio/sfx/throw_healing_item.mp3");
+
+      for (const i in enemies_hit){
+        i.hit(5);
+      }
+      return;
+    
   }
 }
+
+
 
 let player = new Player();
 let shotgun = new Shotgun();
@@ -172,7 +192,7 @@ player.inventory.push(shotgun);
 player.inventory.push(heal);
 player.inventory.push(flashlight);
 
-let current_item = 0;
+let current_item = 3;
 
 let mouse_x;
 let mouse_y;
@@ -184,7 +204,7 @@ function get_mouse_pos(event) {
 
 play_button.addEventListener("click", (e) => {
   document.getElementById("menu").remove();
-});
+})
 
 document.addEventListener("click", (e) => {
   get_mouse_pos(e);
@@ -206,42 +226,60 @@ document.addEventListener("keydown", (event) => {
     swap_to_right_scene();
   }
   if (current_scene_id == SCENE_BAIXO) {
+
     blast_radius = blast_radius_high;
     light_radius = light_radius_high;
     flashlight_radius = flashlight_radius_high;
+
   } else {
+
     blast_radius = blast_radius_low;
     light_radius = light_radius_low;
     flashlight_radius = flashlight_radius_low;
+
+
   }
 });
 
 document.addEventListener("mousemove", (event) => {
   get_mouse_pos(event);
-  if (flashlight.is_on == true) move_flashlight(mouse_x, mouse_y);
+  if (flashlight.is_on == true ) move_flashlight(mouse_x, mouse_y);
 
   flashlight.flash_enemies();
-});
+})
+
+
+
+let update_id;
 
 function update() {
+
   shotgun.chamber_ammo();
   if (flashlight.is_on == true) flashlight.battery_decay();
 
-  update_scene();
-  if (player.health <= 0) {
-    clearInterval();
 
-    game_over = document.createElement("div");
+  update_scene();
+  if (player.health <= 0){
+    clearInterval(update_id);
+
+    game_over = document.createElement('div');
     game_over.id = "game_over";
-    game_over.innerText = "Game Over!";
-    document.body.insertBefore(
-      game_over,
-      document.body.childNodes[0].nextSibling,
-    );
+    game_over.innerHTML = `<h1> Game Over! </h1> 
+<p> Você perdeu dessa vez, mas ainda nâo acabou! </p>
+<p id = "play_again"> Jogar de novo! </p>
+`
+
+    document.body.insertBefore(game_over, document.body.childNodes[0].nextSibling);
+
+    document.getElementById("play_again").addEventListener("click", (e) => {
+      player.health = max_hp;
+      game_over.remove();
+      update_id = setInterval(update, 1000 / fps)
+    });
   }
+
 }
 
-setInterval(update, 1000 / fps);
+update_id = setInterval(update, 1000 / fps);
 
-document.getElementById;
-
+document.getElementById
